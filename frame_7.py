53173c166a59a5a6f44c0a6bbfcb9143c8c1d7da
Michal Pieton <Michal392PP@gmail.com>
2024-06-04 14:57:14 +0200

    update

--- frame_7.py
+++ frame_7.py
@@ -6,10 +6,13 @@ root = Tk()
 
 root.iconbitmap('C:\\Users\\WHK478\\Desktop\\pythong\\Tk\\photo\\rose.ico')
 
-frame = LabelFrame(root, text="This is my frame...", padx=5, pady=5)
+frame = LabelFrame(root, text="This is my frame...", padx=50, pady=50)
 frame.pack(padx=10, pady=10)
 
 b = Button(frame, text="Don't click here!")
-b.pack()
+b2 = Button(frame, text="... and here!")
+b.grid(row=0, column=0)
+b2.grid(row=1, column=1)
+
 
 root.mainloop()
