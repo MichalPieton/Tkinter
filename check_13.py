fb9d2ac05eb36b4b0880f894f93838f245aec4f1
Michal Pieton <Michal392PP@gmail.com>
2024-06-07 15:32:06 +0200

    7.06.2024

---
+++ check_13.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from PIL import Image, ImageTk
+from tkinter import filedialog
+
+root = Tk()
+root.title("Hello world")
+root.iconbitmap("photo/rose.ico")
+
+
+root.mainloop()
